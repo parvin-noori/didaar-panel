083ed703981771b09495ccf3f76160e1e042241e
parvin <parvinnoori0@gmail.com>
2024-03-05 17:14:50 +0330

    app context for change lang

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ConfigProvider } from 'antd';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ConfigProvider } from "antd";
+import { AppProvider, useAppContext } from "./contexts/app/app-context.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const MyApp = () => {
+    const { direction } = useAppContext();
+  
+    return (
+      <ConfigProvider direction={direction}>
+        <App />
+      </ConfigProvider>
+    );
+  };
 
-    <App />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppProvider>
+     <MyApp />
+  </AppProvider>
+);
 
-)
