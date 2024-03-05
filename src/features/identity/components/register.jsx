8a2095112b016288689749a5cecdcedec59e9a73
parvin-noori <parvinnoori0@gmail.com>
2024-03-05 09:52:46 +0330

    registerAction and axios

--- src/features/identity/components/register.jsx
+++ src/features/identity/components/register.jsx
@@ -12,6 +12,7 @@ import {
 import { useForm } from "react-hook-form";
 const { Text } = Typography;
 import { Link } from "react-router-dom";
+import { httpService } from "../../../core/http-service";
 
 export default function Register() {
   const {
@@ -116,4 +117,11 @@ export default function Register() {
       </Card>
     </Flex>
   );
+}
+
+export async function registerAction({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const response = await httpService.post("/Users", data);
+  return response.status === 200;
 }
--- src/router.jsx
+++ src/router.jsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
       {
         path: "register",
         element: <Register />,
+        action:registerAction
       },
     ]
 
