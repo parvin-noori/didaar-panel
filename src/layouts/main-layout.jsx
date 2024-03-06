3fe26695dec18608e4acfe997b8d66cfa0f10175
parvin <parvinnoori0@gmail.com>
2024-03-06 08:28:41 +0330

    header

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -1,22 +1,42 @@
-import { Button, Layout } from "antd";
+import { Button, Col, Input, Layout, Row } from "antd";
 import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 const { Header, Footer, Sider, Content } = Layout;
-import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+const { Search } = Input;
+import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
 
 export default function MainLayout() {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  if (!token) {
+    navigate("/login");
+  }
+  const onSearch = (value, _e, info) => console.log(info?.source, value);
   return (
     <div>
       <Layout>
-        <Sider  collapsed={collapsed} style={{backgroundColor:'#fff'}}></Sider>
+        <Sider
+          collapsed={collapsed}
+          style={{ backgroundColor: "#fff" }}
+        ></Sider>
         <Layout>
-          <Header style={{backgroundColor:'#fff'}}>
-            <Button
-              type="text"
-              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-              onClick={()=>setCollapsed(!collapsed)}
-            ></Button>
+          <Header style={{ backgroundColor: "red" }}>
+            <Row align="center" justify="flex-start">
+              <Col span={1}>
+                {" "}
+                <Button
+                  type="text"
+                  icon={collapsed ? <MenuOutlined /> : <CloseOutlined />}
+                  onClick={() => setCollapsed(!collapsed)}
+                ></Button>
+              </Col>
+              <Col span={6}>
+                {" "}
+                <Search placeholder="جستجو" allowClear onSearch={onSearch} />
+              </Col>
+            </Row>
           </Header>
 
           {/* <Content></Content> */}
