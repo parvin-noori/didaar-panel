82b0b4a40ca45ec987352d4d4d7eab6199313553
parvin <parvinnoori0@gmail.com>
2024-03-06 08:08:48 +0330

    main layout

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -1,16 +1,27 @@
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-const { Header, Footer, Content } = Layout;
+const { Header, Footer, Sider, Content } = Layout;
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
 
 export default function MainLayout() {
   const [collapsed, setCollapsed] = useState(false);
   return (
     <div>
       <Layout>
-        <Header></Header>
-        <Content></Content>
-        <Footer></Footer>
+        <Sider  collapsed={collapsed} style={{backgroundColor:'#fff'}}></Sider>
+        <Layout>
+          <Header style={{backgroundColor:'#fff'}}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={()=>setCollapsed(!collapsed)}
+            ></Button>
+          </Header>
+
+          {/* <Content></Content> */}
+          {/* <Footer></Footer> */}
+        </Layout>
       </Layout>
     </div>
   );
